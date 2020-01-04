4f8c26be6164ae36f1f6e5dff8bc99425de865b5
Aman Awdhani <Amanawdhani19@gmail.com>
2020-01-04 17:37:33 +0530

    Add files via upload

---
+++ Bezier Curve in Unity2D/Assets/bezier.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bezier : MonoBehaviour
+{
+    public List<Transform> points;
+    Vector2 gizmosPos;
+
+    private void OnDrawGizmos()
+    {
+        for (float t = 0; t <= 1; t += .02f)
+        {
+
+            gizmosPos = Mathf.Pow(1 - t, 3) * points[0].position + 3 * Mathf.Pow(1 - t, 2) * t * points[1].position +
+                        3 * (1 - t) * Mathf.Pow(t, 2) * points[2].position + Mathf.Pow(t, 3) * points[3].position;
+
+            Gizmos.DrawSphere(gizmosPos, .2f);
+        }
+        Gizmos.DrawLine(points[0].position , points[1].position);
+        Gizmos.DrawLine(points[2].position, points[3].position);
+    }
+}
